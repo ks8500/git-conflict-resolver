357485d448b3aa06525bd47ce6ed76220c31e2f7
ks8500 <k.sravan.8500@gmail.com>
2023-04-11 20:21:58 +0530

    a

---
+++ src/main/java/com/kurra/GitConflictResolverApplication.java
@@ -0,0 +1,13 @@
+package com.kurra;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitConflictResolverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitConflictResolverApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kurra/GitConflictResolverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kurra;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitConflictResolverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
