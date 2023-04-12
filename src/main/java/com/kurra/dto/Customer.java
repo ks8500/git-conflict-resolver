206686a2aaee2e53320b8c1591c234f94f24e7f4
ks8500 <k.sravan.8500@gmail.com>
2023-04-12 10:13:33 +0530

    a

--- src/main/java/com/kurra/dto/Customer.java
+++ src/main/java/com/kurra/dto/Customer.java
@@ -14,45 +14,45 @@ public class Customer {
     private int age;
     private String location;
     
-//	public Customer(int customerId, String name, int age, String location) {
-//		super();
-//		this.customerId = customerId;
-//		this.name = name;
-//		this.age = age;
-//		this.location = location;
-//	}
-//	public int getCustomerId() {
-//		return customerId;
-//	}
-//	public void setCustomerId(int customerId) {
-//		this.customerId = customerId;
-//	}
-//	public String getName() {
-//		return name;
-//	}
-//	public void setName(String name) {
-//		this.name = name;
-//	}
-//	public int getAge() {
-//		return age;
-//	}
-//	public void setAge(int age) {
-//		this.age = age;
-//	}
-//	public String getLocation() {
-//		return location;
-//	}
-//	public void setLocation(String location) {
-//		this.location = location;
-//	}
-//	@Override
-//	public String toString() {
-//		return "Customer [customerId=" + customerId + ", name=" + name + ", age=" + age + ", location=" + location
-//				+ "]";
-//	}
-//	public Customer() {
-//		super();
-//	}
+	public Customer(int customerId, String name, int age, String location) {
+		super();
+		this.customerId = customerId;
+		this.name = name;
+		this.age = age;
+		this.location = location;
+	}
+	public int getCustomerId() {
+		return customerId;
+	}
+	public void setCustomerId(int customerId) {
+		this.customerId = customerId;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+	public String getLocation() {
+		return location;
+	}
+	public void setLocation(String location) {
+		this.location = location;
+	}
+	@Override
+	public String toString() {
+		return "Customer [customerId=" + customerId + ", name=" + name + ", age=" + age + ", location=" + location
+				+ "]";
+	}
+	public Customer() {
+		super();
+	}
 
     
     
